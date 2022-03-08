1333569bbbb01441606cf4ca3b435f777347d68b
KarolynMorrow <97045272+KarolynMorrow@users.noreply.github.com>
2022-03-08 16:26:59 -0500

    Base commit

---
+++ src/main/java/org/launchcode/KBudget/KBudgetApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.KBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/KBudget/KBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.KBudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

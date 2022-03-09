83926e05e942d22107b8ead1930c87db05548af8
KarolynMorrow <97045272+KarolynMorrow@users.noreply.github.com>
2022-03-09 16:56:46 -0500

    merge

--- src/main/java/org/launchcode/KBudget/Controllers/MainController.java
+++ src/main/java/org/launchcode/KBudget/Controllers/MainController.java
@@ -1,15 +1,18 @@
 package org.launchcode.KBudget.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class MainController {
 
-    @GetMapping("")
-    public String showBudgetPage(){
-        System.out.println("main controller");
+//    @RequestMapping("")
+    @GetMapping
+    public String index(Model model){
+        //System.out.println("main controller");
         return "index";
     }
 }

3efa48f3cd3b871517d729c85eac7798901d0998
KarolynMorrow <97045272+KarolynMorrow@users.noreply.github.com>
2022-03-09 19:39:34 -0500

    Added bills template

--- src/main/java/org/launchcode/KBudget/Controllers/BillsController.java
+++ src/main/java/org/launchcode/KBudget/Controllers/BillsController.java
@@ -3,14 +3,14 @@ package org.launchcode.KBudget.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BillsController {
 
-    @GetMapping("bills")
-    @ResponseBody
+    @RequestMapping("bills")
     public String bill(){
-        return "this is a bill";
+        return "bills/index";
     }
 }

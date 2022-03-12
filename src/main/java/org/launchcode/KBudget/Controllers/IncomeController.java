4088b252cd8f82d888d76bc4d4b2f3e3e1358ee4
KarolynMorrow <97045272+KarolynMorrow@users.noreply.github.com>
2022-03-12 09:48:01 -0500

    Income updated with form

--- src/main/java/org/launchcode/KBudget/Controllers/IncomeController.java
+++ src/main/java/org/launchcode/KBudget/Controllers/IncomeController.java
@@ -1,15 +1,46 @@
 package org.launchcode.KBudget.Controllers;
 
+
+import org.launchcode.KBudget.Models.Income;
+import org.launchcode.KBudget.Models.data.BillsRepository;
+import org.launchcode.KBudget.Models.data.IncomeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
+@RequestMapping("incomes")
 public class IncomeController {
 
-    @GetMapping("income")
-    public String Income() {
-        return "income/index";
+    @Autowired
+    private IncomeRepository incomeRepository;
+
+    @GetMapping("")
+    public String index(Model model){
+        model.addAttribute("title", "My Incomes");
+        model.addAttribute("bill", incomeRepository.findAll());
+        return "incomes/index";
+    }
+    //lives at /incomes/add
+    @GetMapping("add")
+    public String displayBillForm(Model model){
+        model.addAttribute("title", "Add Income");
+        model.addAttribute(new Income());
+        return "incomes/add";
     }
 
+    //lives at /incomes/add
+    @PostMapping("add")
+    public String processAddBillForm(@ModelAttribute @Valid Income newIncome, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("errorMsg","Incorrect input");
+            return "incomes/add";
+        }
+        incomeRepository.save(newIncome);
+        return "redirect:/incomes";
+    }
 }
